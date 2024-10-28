89f19565a6c41a699f898e88365717457b2e430c
geno3a <57619319+geno3a@users.noreply.github.com>
2024-10-28 18:32:52 +0200

    Vlad (#4)
    * test
    
    * added the navbar and button for changing the theme
    
    * added minor fix
    
    * added database and started to develop wizard page
    
    * added wizard page for updating currency and added support for mobile versions

---
+++ spendsmart/lib/currencies.ts
@@ -0,0 +1,7 @@
+export const Currencies = [
+  { value: "UAH", label: "₴ Hryvnia", locale: "de-DE" },
+  { value: "USD", label: "$ Dollar", locale: "en-US" },
+  { value: "EUR", label: "€ Euro", locale: "de-DE" },
+];
+
+export type Currency = (typeof Currencies)[0];
